a050c3446b77cb786be32dac1a75a66f155e0cb5
Daniel Puentes <dpuentesf1@ucentral.edu.co>
2023-10-31 14:04:15 -0500

    Se arreglaron errores logicos y malas paracticas en la estructura del codigo del archivo js y se realizaron diversas estructuras de control

--- logic.js/Script.js
+++ logic.js/Script.js
@@ -3,13 +3,23 @@ function getComputerChoice(){
     return num;
 }
 function traslateSelectionPlayer(playerSelection){
+    if(playerSelection ==='piedra'){
+        return 1;
+    }else if(playerSelection === 'papel'){
+        return 2;
+    }else if(playerSelection === 'tijera'){
+        return 3;
+    }
+}
+
+function checkingSelectionPlayer(playerSelection){
     let stringMin = playerSelection.toLowerCase();
     if(stringMin ==='piedra'){
-        return 1;
+        return stringMin;
     }else if(stringMin === 'papel'){
-        return 2;
+        return stringMin;
     }else if(stringMin === 'tijera'){
-        return 3;
+        return stringMin;
     }else{
         return null;
     }
@@ -20,15 +30,8 @@ function checkSelection(){
     let date = " ";
     while(vali === false){
         date = prompt("Ingrese por favor su elección Piedra, Papel o Tijera:");
-        date = traslateSelectionPlayer(date);
-        if(date === 1){
-            date = "piedra";
-            vali = true;
-        }else if(date === 2){
-            date = "papel";
-            vali = true;
-        }else if(date === 3){
-            date = "tijera";
+        date = checkingSelectionPlayer(date);
+        if(date === "piedra" || date === "papel"|| date === "tijera"){
             vali = true;
         }else{
             window.alert("Ha introdicido mal su selección.\n por favor buelava a intentarlo.");
@@ -40,38 +43,77 @@ function checkSelection(){
 function playRound(playerSelection, computerSelection) {
     let getNewSelectionPleyer = traslateSelectionPlayer(playerSelection);
     if(getNewSelectionPleyer === null){
-        retrun `No se puede juar, ya que el jugador introdujo mal su elección: ${playerSelection}`;
+        retrun `No se puede juar, ya que el jugador introdujo mal su elección: ${playerSelection}.`;
     }else{
         if(getNewSelectionPleyer === 1 && computerSelection === 1){
-            return "Empate: Roca contra Roca.";
+            console.log("Empate: Piedra contra Piedra.");
+            return 0;
         }else if (getNewSelectionPleyer === 1 && computerSelection === 2) {
-            return "La conputadora gana: Papel contra Roca.";
+            console.log("La conputadora gana: Papel contra Piedra.");
+            return 1;
         }else if(getNewSelectionPleyer === 1 && computerSelection === 3){
-            return "El jugador gana: Roca contra Tijera.";
+            console.log("El jugador gana: Piedra contra Tijera.");
+            return 2;
         }else if(getNewSelectionPleyer === 2 && computerSelection === 2){
-            return "Empate: Papel contra Papel.";
+            console.log("Empate: Papel contra Papel.");
+            return 0;
         }else if(getNewSelectionPleyer === 2 && computerSelection === 3){
-            return "La conputadora gana: Tijera contra Papel.";
+            console.log("La conputadora gana: Tijera contra Papel.");
+            return 1;
         }else if(getNewSelectionPleyer === 3 && computerSelection === 3){
-            return "Empate: Tijera contra Tijera.";
+            console.log("Empate: Tijera contra Tijera.");
+            return 0;
         }else if(getNewSelectionPleyer === 2 && computerSelection === 1){
-            return "El jugador gana: Paple contra Roca.";
+            console.log("El jugador gana: Paple contra Piedra.");
+            return 2;
         }else if(getNewSelectionPleyer === 3 && computerSelection === 2){
-            return "El jugador gana: Tijera contra Papel.";
+            console.log("El jugador gana: Tijera contra Papel.");
+            return 2;
         }else if(getNewSelectionPleyer === 3 && computerSelection === 1){
-            return "La conputadora gana: Piedra contra Tijera.";
+            console.log("La conputadora gana: Piedra contra Tijera.");
+            return 1;
         }
     }
+}
 
 function Game(){
     let increase = 0;
-    for (let i = 0; i <5; i++){
+    let winsPlayer = 0;
+    let winsPC = 0;
+    let tie = 0;
+    for (let i = 0; i < 5; i++){
         const playerSelection = checkSelection();
         const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
+        let result = playRound(playerSelection, computerSelection);
         increase += 1;
+        switch(result){
+            case 0:
+                tie += 1;
+            break;
+            case 1:
+                winsPC += 1;
+            break;
+            case 2:
+                winsPlayer += 1;
+            break;    
+        }
     }
-    // Ahora, tengo que identificar quien gana y contar cuantas veces gana cada quien. Además, hay que tener en cuenta que al final de este proceso se debe mostrar en la consola quien gana. Tambien hay que ver por qué no esta llamanado a la función Game() cuando la llamo desde la consola.
+    console.log(`La cantidad de emaptes es: ${tie}; La cantidad de victorias de la PC: ${winsPC}; La cantidad de victorias del jugador: ${winsPlayer}.`);
+    if(winsPlayer < winsPC){
+        console.log("Gana la computadora.");
+    }else if (winsPlayer > winsPC){
+        console.log("Gana el jugador.");
+    }else if(winsPlayer === winsPC){
+        console.log("Termina en empate.");
+    }
 }
 
-}
+window.alert("Primero para poder jugar presiona las sigientes teclas para abrir la consola:\n contol+shift+j.");
+window.alert("Segundo, para empezar a juagar, debes conocer las reglas.");
+window.alert("1. Solo se puede jugar entre dos: Usted y la PC.");
+window.alert("2. Para este juego, Existen 3 movimientos: Piedra, papel o tijera.");
+window.alert("3. La forma en que interactua los tres movimientos son los siguientes: \n 3.1. La piedra le gana a la tijera. \n 3.2. La tijera le gana al papel.\n 3.3. El pale le gana a la piedra.");
+window.alert("4. Si hay un empate no se suman puntos para los jugadores.");
+window.alert("5. La cantidad de rondas son 5.");
+window.alert("Ahora bien y para finalizar, espero que disfrutes el juego y le des un like a este proyecto.");
+console.log(Game());
